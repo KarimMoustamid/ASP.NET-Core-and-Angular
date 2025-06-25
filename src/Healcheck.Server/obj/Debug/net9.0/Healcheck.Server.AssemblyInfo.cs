8355fa1f6e73d064da59cf3c25b8f0c7011e6bb0
KarimMoustamid <moustamid.teck@gmail.com>
2025-06-24 21:09:11 -0600

    update assembly informational version to reflect latest changes

--- src/Healcheck.Server/obj/Debug/net9.0/Healcheck.Server.AssemblyInfo.cs
+++ src/Healcheck.Server/obj/Debug/net9.0/Healcheck.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Healcheck.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39527f5daa70af064aa2ae868eee06851e834bf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aba0a53e7b7fed6d86cb7ae2c69106ae05ce663")]
 [assembly: System.Reflection.AssemblyProductAttribute("Healcheck.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Healcheck.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
